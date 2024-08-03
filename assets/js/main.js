e4e388b487b6cd1f82cca386067436799143595c
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2024-08-03 16:43:42 +0300

    0

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,12 @@ footerWrapperBtns.forEach((btn) => {
     }
   });
 });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
